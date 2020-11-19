fc620f6e7e1343b36341d1ae5e46df611a1418a1
Ethan <em300@hw.ac.uk>
2020-11-19 18:12:18 +0000

    menu done and bug fixed

--- Client/js/placeholder.js
+++ Client/js/placeholder.js
@@ -1,5 +1,12 @@
 const $startButton = document.getElementById("startButton")
+const $background = document.getElementById('background')
+const $playerName = document.getElementById('playerName')
+
 
 $startButton.onclick = () => {
-    $startButton.style.display = "none"
+    $startButton.style.display = 'none'
+    $playerName.style.display = 'none'
+    $background.src = "Img/LoadingScreens/Wallpaper.jpg" 
 }
+
+   
