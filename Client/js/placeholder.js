1921350a48099023c1a5af9045befcb526fda950
JosephP98 <71353546+JosephP98@users.noreply.github.com>
2020-11-19 14:59:40 +0000

    Update placeholder.js

--- Client/js/placeholder.js
+++ Client/js/placeholder.js
@@ -1,1 +1,15 @@
-console.log("here to fill space for a bit. :)");
+var playButton = document.getElementById("startButton");
+
+//document.getElementById("gameWindow").style.opacity = 0;
+
+
+startButton.onclick = function(){ // when start button is clicked
+    document.getElementById("startButton").style.opacity = 0; //hide all menu elements
+    document.getElementById("titleCard").style.opacity = 0; // hide title card
+    document.getElementById("background").style.opacity = 0; // hide background
+    //document.getElementById("gameWindow").style.opacity = 100; // Change game to fully visible
+};
+
+function getuserName(){
+    var userName = document.getElementById("nameInput").value;
+};
