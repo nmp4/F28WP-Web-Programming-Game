14dbd4a59cea631dc10c2b3f89f7ddeceb6faeaf
JosephP98 <71353546+JosephP98@users.noreply.github.com>
2020-11-20 15:12:30 +0000

    Update placeholder.js

--- Client/js/placeholder.js
+++ Client/js/placeholder.js
@@ -1,12 +1,20 @@
 const $startButton = document.getElementById("startButton")
 const $background = document.getElementById('background')
-const $playerName = document.getElementById('playerName')
+const $playerNameInput = document.getElementById('playerNameInput')
+const $playerNameButton = document.createElement('playerNameButton')
 
 
 $startButton.onclick = () => {
-    $startButton.style.display = 'none'
-    $playerName.style.display = 'none'
-    $background.src = "Img/LoadingScreens/Wallpaper.jpg" 
+    $startButton.style.display = 'none';
+    $playerNameInput.style.display = 'none';
+    $background.src = "Img/LoadingScreens/Wallpaper.jpg";
 }
+
+$playerNameButton.onclick = () => {
+    var $playerName = document.getElementById("playerNameInput").value;
+    $playerNameInput.style.display = 'none';
+    $playerNameButton.style.display = 'none';
+    return $playerName;
+};
 
    
