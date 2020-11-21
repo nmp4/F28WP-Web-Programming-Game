aa9ded6132c8808f85d23191737f375e50d3656f
JosephP98 <71353546+JosephP98@users.noreply.github.com>
2020-11-21 11:58:27 +0000

    Update placeholder.js
    added basic movement function for a css circle

--- Client/js/placeholder.js
+++ Client/js/placeholder.js
@@ -17,4 +17,42 @@ $playerNameButton.onclick = () => {
     return $playerName;
 };
 
-   
+window.addEventListener('Load', () => {
+    circle.style.position = 'absolute';
+    circle.style.left = 960;
+    circle.style.top = 540;
+});
+
+window.addEventListener('keydown', (e) => {switch (e.key) { //Movement fuction detects when arrow keys are
+    case 'ArrowLeft':
+        circle.style.left = parseInt(circle.style.left) - moveBy + 'px'
+        break;
+    case 'ArrowRight':
+        circle.style.left = parseInt(circle.style.left) + moveBy + 'px'
+        break;
+    case 'ArrowUp':
+        circle.style.top = parseInt(circle.style.top) - moveBy + 'px'
+        break;
+    case 'ArrowDown':
+        circle.style.top = parseInt($circle.style.top) + moveBy + 'px'
+        break;
+    case 'ArrowDown' && 'ArrowRight':
+        circle.style.top = parseInt(circle.style.top) + moveBy + 'px'
+        circle.style.left = parseInt(circle.style.left) + moveBy + 'px'
+        break;
+    case 'ArrowDown' && 'ArrowLeft':
+        circle.style.top = parseInt(circle.style.top) + moveBy + 'px'
+        circle.style.left = parseInt(circle.style.left) - moveBy + 'px'
+        break;
+    case 'ArrowUp' && 'ArrowRight':
+        circle.style.top = parseInt(circle.style.top) - moveBy + 'px'
+        circle.style.left = parseInt(circle.style.left) + moveBy + 'px'
+        break;
+    case 'ArrowUp' && 'ArrowLeft':
+        circle.style.top = parseInt(circle.style.top) - moveBy + 'px'
+        circle.style.top = parseInt(circle.style.left) - moveBy + 'px'
+        break;
+
+    
+
+}})   
