60ca49ede86ddc9c39b56679071a2ba4317fedce
nmp4 <nmp4@hw.ac.uk>
2020-11-23 11:52:41 +0100

    added socket.io functionality

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ let app = express();
 let serv = require("http").Server(app);
 
 app.get("/", function(req, res) {
-    	res.sendFile(__dirname + "/client/game.html");
+    	res.sendFile(__dirname + "/Client/game.html");
 });
 
-app.use("/client", express.static(__dirname + "/client"));
+app.use("/Client", express.static(__dirname + "/Client"));
 
 serv.listen(process.env.PORT || 80);
+console.log("Server Started");
 
+let io = require("socket.io")(serv, {});
+
+io.sockets.on("connection", function(socket){
+    console.log("socket connection");
+});
