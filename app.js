1e2a6f958233e543cce757c9cef1c427f1323c0c
nmp4 <nmp4@hw.ac.uk>
2020-11-23 12:13:12 +0100

    Added more server functionality

--- app.js
+++ app.js
@@ -15,5 +15,17 @@ console.log("Server Started");
 let io = require("socket.io")(serv, {});
 
 io.sockets.on("connection", function(socket){
+    socket.id = Math.floor(Math.random() * 10000);
+
+    SOCKETLIST[socket.id] = socket;
+
     console.log("socket connection");
 });
+
+let SOCKETLIST = {};
+
+setInterval(function(){
+    for(let i in SOCKETLIST){
+        let socket = SOCKETLIST[i];
+    }
+},1000/30);
